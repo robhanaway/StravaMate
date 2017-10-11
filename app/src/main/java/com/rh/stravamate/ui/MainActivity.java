1e4bae795737e9e051e0d8513d41d6dca986344a
robert.hanaway <robert.hanaway@synchronoss.com>
2017-10-11 13:23:51 +0100

    Added drawer

--- app/src/main/java/com/rh/stravamate/ui/MainActivity.java
+++ app/src/main/java/com/rh/stravamate/ui/MainActivity.java
@@ -6,6 +6,9 @@ import android.app.Fragment;
 import android.app.FragmentTransaction;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.widget.DrawerLayout;
+import android.view.View;
 
 
 import com.rh.stravamate.R;
@@ -26,10 +29,42 @@ import retrofit2.Response;
 public class MainActivity extends BaseActivity implements
         AuthFragment.OnFragmentInteractionListener,
         ActivityFragment.OnListFragmentInteractionListener{
+
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mDrawerToggle;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerToggle = new ActionBarDrawerToggle(
+                this,                  /* host Activity */
+                mDrawerLayout,         /* DrawerLayout object */
+                R.mipmap.ic_launcher,
+                R.string.app_name,  /* "open drawer" description */
+                R.string.app_name  /* "close drawer" description */
+        ) {
+
+            /** Called when a drawer has settled in a completely closed state. */
+            public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+                getSupportActionBar().setTitle("");
+            }
+
+            /** Called when a drawer has settled in a completely open state. */
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                getSupportActionBar().setTitle("");
+            }
+        };
+
+        // Set the drawer toggle as the DrawerListener
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setLogo(R.drawable.actionbar_drawer);
+        getSupportActionBar().setDisplayUseLogoEnabled(true);
         logging.d(getTag(), "onCreate");
 
         loadMe();
